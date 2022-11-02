8f7c4ed9c0d98e783f15d6a480c244d7db51ee39
Lodenou <denoual.loic.dl@gmail.com>
2022-11-02 14:03:16 +0100

    Set theme and proper layouts

--- app/src/main/java/com/lodenou/go4lunchv4/ui/activities/MainActivity.java
+++ app/src/main/java/com/lodenou/go4lunchv4/ui/activities/MainActivity.java
@@ -1,33 +1,54 @@
 package com.lodenou.go4lunchv4.ui.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.lodenou.go4lunchv4.R;
+import com.lodenou.go4lunchv4.databinding.ActivityMainBinding;
 import com.lodenou.go4lunchv4.ui.fragment.listview.ListViewFragment;
 import com.lodenou.go4lunchv4.ui.fragment.map.MapFragment;
 import com.lodenou.go4lunchv4.ui.fragment.workmates.WorkmatesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mBinding;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
         setBottomNavigationView();
+        setToolBar();
+        setDrawerLayout();
+        setNavigationViewClickListener();
     }
 
     private void setBottomNavigationView(){
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(navListener);
-
-        // as soon as the application opens the first
-        // fragment should be shown to the user
-        // in this case it is map fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
+        mBinding.bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(mBinding.fragmentContainer.getId(), new MapFragment()).commit();
     }
 
     private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
@@ -50,4 +71,51 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     };
+
+    // 1 - Configure Toolbar
+    private void setToolBar(){
+        this.toolbar = mBinding.activityMainToolbar;
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+    }
+
+    // 2 - Configure Drawer Layout
+    private void setDrawerLayout(){
+        this.drawerLayout = mBinding.activityMainDrawerLayout;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    // 3 - Configure NavigationView
+    private void setNavigationViewClickListener(){
+        this.navigationView = mBinding.activityMainNavView;
+//        navigationView.setItemTextColor(ColorStateList.valueOf(Color.WHITE));
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.nav_yourlunch:
+                        Intent myIntent = new Intent(MainActivity.this, DetailActivity.class);
+                        startActivity(myIntent);
+                        break;
+                    case R.id.nav_settings:
+                        //todo onclicklistener
+                        break;
+                    case R.id.nav_logout:
+                        //todo onclicklistener
+                        break;
+                    case R.id.chat:
+                        //todo onclicklistener
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
 }
