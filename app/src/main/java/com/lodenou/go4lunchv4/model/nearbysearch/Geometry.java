fb12a287c5939c2f8b06a83d438f35f8d33edbab
Lodenou <denoual.loic.dl@gmail.com>
2022-11-03 11:24:16 +0100

    Models for detail & nearbysearch api calls

---
+++ app/src/main/java/com/lodenou/go4lunchv4/model/nearbysearch/Geometry.java
@@ -0,0 +1,33 @@
+package com.lodenou.go4lunchv4.model.nearbysearch;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class Geometry {
+
+    @SerializedName("location")
+    @Expose
+    private Location location;
+    @SerializedName("viewport")
+    @Expose
+    private Viewport viewport;
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(Viewport viewport) {
+        this.viewport = viewport;
+    }
+
+}
