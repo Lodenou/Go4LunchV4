94be59d426d90175ef596176f284553df407a485
Lodenou <denoual.loic.dl@gmail.com>
2022-11-11 17:33:26 +0100

    public function to get restaurant details

--- app/src/main/java/com/lodenou/go4lunchv4/data/RestaurantRepository.java
+++ app/src/main/java/com/lodenou/go4lunchv4/data/RestaurantRepository.java
@@ -1,10 +1,11 @@
 package com.lodenou.go4lunchv4.data;
 
+
 import androidx.lifecycle.MutableLiveData;
-import com.lodenou.go4lunchv4.model.Restaurant;
+
+import com.lodenou.go4lunchv4.model.detail.DetailResult;
 import com.lodenou.go4lunchv4.model.nearbysearch.NearbySearchResults;
 import com.lodenou.go4lunchv4.model.nearbysearch.Result;
-
 import java.util.ArrayList;
 import java.util.List;
 import io.reactivex.Observer;
@@ -16,7 +17,10 @@ public class RestaurantRepository {
 
     private static RestaurantRepository instance;
     private ArrayList<Result> dataset = new ArrayList<>();
-    MutableLiveData<List<Result>> data = new MutableLiveData<>();
+    private com.lodenou.go4lunchv4.model.detail.Result mRestaurant;
+    MutableLiveData<List<Result>> dataNearby = new MutableLiveData<>();
+    MutableLiveData<com.lodenou.go4lunchv4.model.detail.Result> dataDetail =
+            new MutableLiveData<com.lodenou.go4lunchv4.model.detail.Result>();
 
     public static RestaurantRepository getInstance(){
         if (instance == null) {
@@ -57,11 +61,10 @@ public class RestaurantRepository {
 
                     @Override
                     public void onNext(NearbySearchResults nearbySearchResults) {
-//                        dataset.clear();
-
-                        //fixme change the type Result to Restaurant
+                        dataset.clear();
                         dataset.addAll(nearbySearchResults.getResults());
-                        data.setValue(dataset);
+                        dataNearby.setValue(dataset);
+
                     }
 
                     @Override
@@ -74,8 +77,35 @@ public class RestaurantRepository {
 
                     }
                 });
-        return data;
+        return dataNearby;
     }
 
+    public MutableLiveData<com.lodenou.go4lunchv4.model.detail.Result> getRestaurantDetails(String restaurantId){
+        Go4LunchApi.retrofit.create(Go4LunchApi.class).getPlaceDetails(restaurantId)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<DetailResult>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
+                    }
+
+                    @Override
+                    public void onNext(DetailResult detailResult) {
+                        mRestaurant  = detailResult.getResult();
+                        dataDetail.setValue(mRestaurant);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+        return dataDetail;
+    }
 }
