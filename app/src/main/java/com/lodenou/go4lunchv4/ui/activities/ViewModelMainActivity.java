86b489b6b246e4ec3796c4732a2a51995dd3948b
Lodenou <denoual.loic.dl@gmail.com>
2022-12-20 15:23:45 +0100

    get user from main activity => your lunch button purpose

--- app/src/main/java/com/lodenou/go4lunchv4/ui/activities/ViewModelMainActivity.java
+++ app/src/main/java/com/lodenou/go4lunchv4/ui/activities/ViewModelMainActivity.java
@@ -1,14 +1,29 @@
 package com.lodenou.go4lunchv4.ui.activities;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
 import com.lodenou.go4lunchv4.data.UserRepository;
+import com.lodenou.go4lunchv4.model.User;
 
 public class ViewModelMainActivity extends ViewModel {
 
     private UserRepository mUserRepository;
+    private LiveData<User> mUserLiveData;
 
-    public void init(){
-        mUserRepository = mUserRepository.getInstance();
-//        mUserRepository.createUserInFirestore();
+    public void init() {
+//        if (mUserLiveData != null) {
+//            return;
+//        }
+        mUserRepository = UserRepository.getInstance();
+        mUserLiveData = mUserRepository.getUser();
+    }
+
+    public LiveData<User> getUser() {
+        return mUserLiveData;
+    }
+
+    public void fetchUser() {
+        mUserRepository.getUser();
     }
 }
