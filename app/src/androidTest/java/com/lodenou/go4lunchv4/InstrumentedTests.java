2a7a5408ce7cc5171aba3cbead275231fd39513d
Lodenou <denoual.loic.dl@gmail.com>
2023-01-08 18:42:45 +0100

    Add landscape compatibility

---
+++ app/src/androidTest/java/com/lodenou/go4lunchv4/InstrumentedTests.java
@@ -0,0 +1,157 @@
+package com.lodenou.go4lunchv4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import com.lodenou.go4lunchv4.ui.activities.ConnexionActivity;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTests {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.lodenou.go4lunchv4", appContext.getPackageName());
+    }
+
+//    @Rule
+//    public ActivityScenarioRule<ConnexionActivity> mActivityScenarioRule =
+//            new ActivityScenarioRule<>(ConnexionActivity.class);
+//
+//    @Rule
+//    public GrantPermissionRule mGrantPermissionRule =
+//            GrantPermissionRule.grant(
+//                    "android.permission.ACCESS_FINE_LOCATION",
+//                    "android.permission.ACCESS_COARSE_LOCATION",
+//                    "android.permission.INTERNET");
+//
+//    @Test
+//    public void testMenus() {
+//        ViewInteraction appCompatButton = onView(
+//                allOf(withId(R.id.google_button), withText("Sign in with google"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withClassName(is("android.widget.LinearLayout")),
+//                                        4),
+//                                0),
+//                        isDisplayed()));
+//        appCompatButton.perform(click());
+//
+//        ViewInteraction bottomNavigationItemView = onView(
+//                allOf(withId(R.id.list_view), withContentDescription("List View"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(R.id.bottom_navigation),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        bottomNavigationItemView.perform(click());
+//
+//        ViewInteraction bottomNavigationItemView2 = onView(
+//                allOf(withId(R.id.workmates), withContentDescription("Workmates"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(R.id.bottom_navigation),
+//                                        0),
+//                                2),
+//                        isDisplayed()));
+//        bottomNavigationItemView2.perform(click());
+//    }
+//
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+
+    @Rule
+    public ActivityScenarioRule<ConnexionActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(ConnexionActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION");
+
+    @Test
+    public void connexionActivityTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.google_button), withText("Sign in with google"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        4),
+                                0),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
